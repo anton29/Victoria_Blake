a53d06f6cfb090a6d6e3779d697e4e476a524bc4
anton29 <abr8892@yahoo.com>
2015-09-01 15:31:26 -0400

    9_1

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -1,7 +1,24 @@
+#	Copyright 2015, Google, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#! /bin/bash
+
+# [START startup]
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
 
 # Install logging monitor and configure it to pickup application logs
+# [START logging]
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 
 cat >/etc/google-fluentd/config.d/nodeapp.conf << EOF
@@ -31,6 +48,7 @@ cat >/etc/google-fluentd/config.d/nodeapp.conf << EOF
 EOF
 
 service google-fluentd restart &
+# [END logging]
 
 # Add nodejs repository (NodeSource)
 curl -sL https://deb.nodesource.com/setup | bash -
@@ -60,7 +78,9 @@ autorestart=true
 user=nodeapp
 environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production"
 EOF
+
 supervisorctl reread
 supervisorctl update
 
 # Application should now be running under supervisor
+# [END startup]
